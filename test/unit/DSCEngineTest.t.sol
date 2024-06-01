751f52cc1148a86c5b165c6abe4324caa9d6764e
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-06-01 10:30:20 +0530

    test zero burn amount

--- test/unit/DSCEngineTest.t.sol
+++ test/unit/DSCEngineTest.t.sol
@@ -172,4 +172,15 @@ contract DSCEngineTest is Test {
         dsce.mintDsc(AMOUNT_TO_MINT);
         vm.stopPrank();
     }
+
+    // burnDsc Tests
+
+    function testRevertsIfBurnAmountIsZero() public {
+        vm.startPrank(USER);
+        ERC20Mock(weth).approve(address(dsce), AMOUNT_COLLATERAL);
+        dsce.depositCollateralAndMintDsc(weth, AMOUNT_COLLATERAL, AMOUNT_TO_MINT);
+        vm.expectRevert(DSCEngine.DSCEngine__NeedsMoreThanZero.selector);
+        dsce.burnDsc(0);
+        vm.stopPrank();
+    }
 }

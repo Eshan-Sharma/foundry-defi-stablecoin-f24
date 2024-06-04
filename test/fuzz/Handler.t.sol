0a59bba768a7533d3dd433c823e04f6eb189938b
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-06-04 14:05:31 +0530

    invariant handler

---
+++ test/fuzz/Handler.t.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {DeployDSC} from "script/DeployDSC.s.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+import {MockV3Aggregator} from "test/mocks/MockV3Aggregator.sol";
+
+contract HandlerTest is Test {}

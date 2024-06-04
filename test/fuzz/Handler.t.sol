b9263a28124bbf247327bc1c02c9ca5f87655918
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-06-04 14:30:44 +0530

    added handler

--- test/fuzz/Handler.t.sol
+++ test/fuzz/Handler.t.sol
@@ -2,11 +2,19 @@
 pragma solidity ^0.8.18;
 
 import {Test} from "forge-std/Test.sol";
-import {DeployDSC} from "script/DeployDSC.s.sol";
-import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
-import {HelperConfig} from "script/HelperConfig.s.sol";
-import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
-import {MockV3Aggregator} from "test/mocks/MockV3Aggregator.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 
-contract HandlerTest is Test {}
+contract Handler is Test {
+    DSCEngine dsce;
+    DecentralizedStableCoin dsc;
+
+    constructor(DSCEngine _dscEngine, DecentralizedStableCoin _dsc) {
+        dsce = _dscEngine;
+        dsc = _dsc;
+    }
+
+    function depositCollateral(address collateral, uint256 amountCollateral) public {
+        dsce.depositCollateral(collateral, amountCollateral);
+    }
+}
--- test/fuzz/OpenInvariantsTest.t.sol
+++ test/fuzz/OpenInvariantsTest.t.sol
@@ -9,6 +9,7 @@ import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {HelperConfig} from "script/HelperConfig.s.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+import {Handler} from "test/fuzz/Handler.t.sol";
 
 contract OpenInvariantsTest is StdInvariant, Test {
     DeployDSC deployer;
@@ -17,12 +18,15 @@ contract OpenInvariantsTest is StdInvariant, Test {
     HelperConfig config;
     address weth;
     address wbtc;
+    Handler handler;
 
     function setUp() external {
         deployer = new DeployDSC();
         (dsc, dsce, config) = deployer.run();
         (,, (weth), (wbtc),) = config.activeNetworkConfig();
-        targetContract(address(dsce));
+        //targetContract(address(dsce));
+        handler = new Handler(dsce, dsc);
+        targetContract(address(handler));
     }
 
     function invariant_protocolMustHaveMoreValueThanTotalSupply() public view {
@@ -35,6 +39,6 @@ contract OpenInvariantsTest is StdInvariant, Test {
         uint256 wethValue = dsce.getUsdValue(weth, totalWETHDepositied);
         uint256 wbtcValue = dsce.getUsdValue(wbtc, totalWBTCDepositied);
 
-        assert(wethValue + wbtcValue > totalSupply);
+        assert(wethValue + wbtcValue >= totalSupply);
     }
 }
